e44bea35d4c7708224181f0dea57c77b877cbaa9
QuickChangeSpidey <akshay.pandey@telus.com>
2022-11-10 14:18:38 -0500

    initial commit

---
+++ Example/Pods/Target Support Files/Pods-uds-ios-theme-telus_Example/Pods-uds-ios-theme-telus_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_uds_ios_theme_telus_Example : NSObject
+@end
+@implementation PodsDummy_Pods_uds_ios_theme_telus_Example
+@end
---
+++ Example/Pods/Target Support Files/uds-ios-theme-telus/uds-ios-theme-telus-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_uds_ios_theme_telus : NSObject
+@end
+@implementation PodsDummy_uds_ios_theme_telus
+@end
